094604b4096c29962bc7166399df85f1b279c01c
Eli2407 <eliana.reyes@soy.sena.edu.co>
2024-06-17 17:20:06 -0500

    SeCreoFormulario

---
+++ TrabajoGit/Vista/frmLOGIN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoGit.Vista
+{
+    public partial class frmLOGIN : Form
+    {
+        public frmLOGIN()
+        {
+            InitializeComponent();
+        }
+    }
+}
